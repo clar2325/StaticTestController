1ab7fcb62afd3daea9f8078a4f3eb24f5a361712
Pierre Abillama <abill001@umn.edu>
2017-01-22 17:26:43 -0600

    Cleaned up version. Changes made to account for the new sensors used, pending changes for force sensor.

--- static_test_driver/static_test_driver.ino
+++ static_test_driver/static_test_driver.ino
@@ -1,3 +1,4 @@
+#include "Adafruit_MAX31855.h"
 #include <SPI.h>
 //#include <SD.h>
 #include <Wire.h>
@@ -6,17 +7,22 @@
 #include <Adafruit_Sensor.h>
 #include <Telemetry.h>
 
-//File dataFile;                                      // tempFile,accFile; 
-//RTC_DS1307 RTC;
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 const int chipSelect=10;                            // Use digital pin 10 as the slave select pin (SPI bus).
-float temp,x,y,z;
+float temp,pressure,x,y,z;
 bool temp_status = false;
 
-#define temp_pin A2
+//Thermocouple pins
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
-// This is the information on the sensor being used. 
-// See the www.vernier.com/products/sensors.
+//Pressure Setup
+#define PRESSURE_CALIBRATION_FACTOR 246.58
+#define PRESSURE_OFFSET 118.33
+#define PRESSURE_PIN 1
+
 
 #define HIGH_RANGE
 
@@ -65,23 +71,30 @@ char data_name[20] = "";
 
 void setup()
 {
+  while (!Serial);
   Serial.begin(230400);
   Serial.println("Initializing...");
-  pinMode(temp_pin, INPUT);
-  temp = analogRead(temp_pin);
-  if (temp == -102 && temp_status) {
+  // wait for MAX chip to stabilize
+  delay(500);
+  //--------------------Set up thermocouple--------------------
+  temp = thermocouple.readCelsius();
+  if (isnan(temp)){
     Serial.println("Temp sensor err");
     temp_status = false;
-  }
-  else if (temp != -102 && !temp_status) {
+  } 
+  else {
     Serial.println("Temp sensor connected");
     temp_status = true;
   }
+  //-------------------Set up pressure sensors--------------------
+  pinMode (PRESSURE_PIN,INPUT);
+  unsigned long loops = 0;
 }
 
-unsigned long loops = 0;
+
 void loop() {
-  
+
+  //--------------Grab Force Data----------------------
   float count = analogRead(A0);
   float voltage = count / 1023 * 5.0;// convert from count to raw voltage
   float force_reading = intercept + voltage * slope; //converts voltage to sensor reading
@@ -93,26 +106,23 @@ void loop() {
     zero_ready = true;
   }
 
+  //---------------Grab Pressure Data-------------------
+  pressure = (analogRead (PRESSURE_PIN)* 5/ 1024.) * PRESSURE_CALIBRATION_FACTOR - PRESSURE_OFFSET;
+  
 
   // --------------Grab Tempdata------------------------ 
 
   // Temp sensors are slow, so alternate taking data from each sensor each loop
   // Why "byIndex"? You can have more than one IC on the same bus. 
-  temp = (analogRead(temp_pin)-512)/5;
-  if (temp == -102 && temp_status) {
+  temp = thermocouple.readCelsius();
+  if (isnan(temp)){
     Serial.println("Temp sensor err");
     temp_status = false;
-  }
-  else if (temp != -102 && !temp_status) {
+  } 
+  else {
     Serial.println("Temp sensor connected");
     temp_status = true;
   }
-  // Send the command to get temperatures for the next loop
-  //sensors.requestTemperatures();
-  
-  // ---Get a new sensor event 
-  //sensors_event_t event;
-  //mma.getEvent(&event);
 
   //---------- Display the results (acceleration is measured in m/s^2)
   
@@ -126,6 +136,7 @@ void loop() {
 
   BEGIN_SEND
   SEND_ITEM(force, force_reading)
+  SEND_ITEM(pressure,pressure)
   //SEND_ITEM(acceleration, x)
   //SEND_GROUP_ITEM(y)
   //SEND_GROUP_ITEM(z)
