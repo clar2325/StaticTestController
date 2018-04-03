5245b84b083f2c4c5027b5c08142e7ab9b924a5a
Pierre Abillama <abill001@umn.edu>
2018-04-02 23:22:55 -0500

    Update sensors.ino

--- static_test_driver/sensors.ino
+++ static_test_driver/sensors.ino
@@ -1,3 +1,51 @@
+void tare_pressure (int pressure_pin) {
+  int index;
+  float pressure;
+  if (pressure_pin == PRESSURE_FUEL) {
+    index = 0;
+    pressure = pressure_fuel;
+  }
+  else if (pressure_pin == PRESSURE_OX) {
+    index = 1;
+    pressure = pressure_ox;
+  }
+  else
+    return;
+  
+  pressure_hist_vals[index][pressure_val_num[index]] = pressure;
+  pressure_val_num[index]++;
+  if (pressure_val_num[index] >= PRESSURE_NUM_HIST_VALS) {
+    pressure_val_num[index] = 0;
+    pressure_zero_ready[index] = true;
+  }
+}
+
+void zero_pressure (int pressure_pin, const char *pressure_name){
+  int index;
+  if (pressure_pin == PRESSURE_FUEL) {
+    index = 0;
+  }
+  else if (pressure_pin == PRESSURE_OX) {
+    index = 1;
+  }
+  else
+    return;
+  
+  if (pressure_zero_ready[index]) {
+      Serial.print(F("Zeroing "));
+      Serial.print(pressure_name);
+      Serial.println(F(" pressure"));
+      pressure_zero_ready[index] = false;
+      pressure_zero_val[index] = 0;
+      for (int i = 0; i < PRESSURE_NUM_HIST_VALS; i++)
+        pressure_zero_val[index] += pressure_hist_vals[index][i];
+      pressure_zero_val[index] /= PRESSURE_NUM_HIST_VALS;
+    }
+    else {
+      Serial.print(pressure_name);
+      Serial.println(F(" pressure zero value not ready"));
+    }
+}
 
 void init_thermocouple(const char *sensor_name, int led, Adafruit_MAX31855 &thermocouple) {
   float result = thermocouple.readCelsius();
