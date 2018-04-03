0364cade1e1277083c4279de54af2d6e209895ba
Pierre Abillama <abill001@umn.edu>
2018-04-03 18:15:00 -0500

    style for mean function

--- static_test_driver/sensors.ino
+++ static_test_driver/sensors.ino
@@ -2,7 +2,7 @@ float mean (const float *data, unsigned int size) {
   float result;
   for (int i = 0; i < size; i++)
       result += data[i];
-  return result /= size;
+  return result / size;
 }
 
 void init_thermocouple(const char *sensor_name, int led, Adafruit_MAX31855 &thermocouple) {
