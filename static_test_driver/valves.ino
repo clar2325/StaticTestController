7ac597dba2431729fc72bc3126add3a713c6fa84
Zachary Clark <115044566+clar2325@users.noreply.github.com>
2022-10-11 16:40:37 -0500

    N-Valve; Kerovent-Valve
    N-pre/N-main and KV-pre/KV-main require assigned pins.

--- static_test_driver/valves.ino
+++ static_test_driver/valves.ino
@@ -1,8 +1,8 @@
 
-const uint8_t valve_pins[] = {34, 33, 32, 31};
+const uint8_t valve_pins[] = {34, 33, 32, 31,-,-,-,-};
 
-const char *valve_names[] = {"Fuel prestage", "Fuel mainstage", "Oxygen prestage", "Oxygen mainstage"};
-const char *valve_telemetry_ids[] = {"fuel_pre_setting", "fuel_main_setting", "ox_pre_setting", "ox_main_setting"};
+const char *valve_names[] = {"Fuel prestage", "Fuel mainstage", "Oxygen prestage", "Oxygen mainstage", "Nitrogen prestage", "Nitrogen mainstage", "Kerovent prestage", "Kerovent mainstage"};
+const char *valve_telemetry_ids[] = {"fuel_pre_setting", "fuel_main_setting", "ox_pre_setting", "ox_main_setting", "N_pre_setting", "N_main_setting", "Kv_pre_setting", "Kv_main_setting"};
 
 void init_engine() {
   for (uint8_t i = 0; i < sizeof(valve_pins); i++) {
